f62af3a6ae92850e1017ff34a0fa7ebf214555fc
Denver Lewis <denverlewis@protonmail.com>
2020-10-07 13:55:57 +0100

    config

--- config.def.h
+++ config.def.h
@@ -9,11 +9,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "xos4 terminus:size=12" };
 static const char dmenufont[]       = "xos4 terminus:size=12";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#22242d";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#5294e2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
